459d4291a0bba5c9a0348883b9188c008530a6b2
alexnesov <alexandrenesovic@gmail.com>
2021-05-09 10:26:08 +0200

    __repr__ /b

--- Python_version/crypto_hash.py
+++ Python_version/crypto_hash.py
@@ -1,4 +1,12 @@
+import hashlib
+
 def crypto_hash(data):
     """
     Return a sha-256 hash of the given data.
     """
+    return hashlib.sha256(data)
+
+
+def main():
+    print(f"crypto_hash('foo'): {crypto_hash('foo')}")
+
