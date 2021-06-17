4cbe481709378620b2afb2031705ee3e3747104f
alexnesov <alexandrenesovic@gmail.com>
2021-06-17 23:42:28 +0200

    hash

--- Python_version/crypto_hash.py
+++ Python_version/crypto_hash.py
@@ -2,24 +2,23 @@ import hashlib
 import json
 
 
-def stringify(data):
-    return json.dumps(data)
+
 
 def crypto_hash(*args):
     """
     Return a sha-256 hash of the given argument.
     """
-    stringified_args = map(stringify, args)
-    print(f'args: {args}')
-    joined_data = ''.join(args)
+    stringified_args = map(lambda data: json.dumps(data), args)
+    print(f'stringified_args: {stringified_args}')
+    joined_data = ''.join(stringified_args)
 
-    print(f'joined_data: {joined_data}')
+    print(f'joined_data: {stringified_args}')
     
     return hashlib.sha256(joined_data.encode('utf-8')).hexdigest()
 
 
 def main():
-    print(f"crypto_hash('one','two','three'): {crypto_hash('one','two','three')}")
+    print(f"crypto_hash('one', 2, [3]): {crypto_hash('one',2,[3])}")
 
 
 if __name__ == '__main__':
