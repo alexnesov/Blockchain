22b1e2857a419554c020e42c4f25b64c731fbb6a
alexnesov <alexandrenesovic@gmail.com>
2021-12-09 21:52:59 +0100

    synchronize peer on startup

--- Python_version/backend/app/__init__.py
+++ Python_version/backend/app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 import os
 import random
+import requests
 
 from backend.blockchain.blockchain import Blockchain
 from backend.pubsub import PubSub
@@ -33,14 +34,17 @@ def route_blockchain_mine():
 
     return jsonify(block.to_json())
 
-
-PORT = 5000
+ROOT_PORT   = 5000
+PORT        = ROOT_PORT
 
 # After doing a "export PEER=True && python3 -m backend.app"
 # there won't be a "OSError: [Errno 98] Address already in use" (port)
 if os.environ.get('PEER') == 'True':
-    PORT = random.randint(5001, 6000)
+    PORT                = random.randint(5001, 6000)
+    result              = requests.get(f'http://localhost:{ROOT_PORT}/blockchain')
+    result_blockchain   = Blockchain.from_json(result.json())
 
+    blockchain.replace_chain(result_blockchain.chain)
 
 
 app.run(port = PORT)
