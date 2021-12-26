2a877d3a16298fb96bce4ab0c5447b0874b16971
alexnesov <alexandrenesovic@gmail.com>
2021-12-26 21:36:43 +0100

    test app init

--- Python_version/backend/scripts/test_app.py
+++ Python_version/backend/scripts/test_app.py
@@ -1,4 +1,5 @@
-import requests
+import requests, time
+from backend.wallet.wallet import Wallet
 
 BASE_URL = 'http://localhost:5000'
 
@@ -11,8 +12,11 @@ def get_blockchain_mine():
     return requests.get(f'{BASE_URL}/blockchain/mine').json()
 
 
-def post_wallet_transaction(recipient, amount):
-    return requests.post(f'{BASE_URL}/wallet')
+def post_wallet_transact(recipient, amount):
+    return requests.post(
+    f'{BASE_URL}/wallet/transact',
+    json = {'recipient': recipient,
+            'amount' : amount}).json()
 
 
 start_blockchain = get_blockchain()
@@ -21,6 +25,17 @@ print(f'start_blockchain: {start_blockchain}')
 
 
 
+recipient = Wallet().address
+
+post_wallet_transact_1 = post_wallet_transact(recipient, 21)
+print(f'\npost_wallet_transact_1:  {post_wallet_transact_1}')
 
 
+post_wallet_transact_2 = post_wallet_transact(recipient, 13)
+print(f'\npost_wallet_transact_2:  {post_wallet_transact_2}')
 
+
+time.sleep(1)
+
+mined_block = get_blockchain_mine()
+print(f'\nmined_block: {mined_block} ')
